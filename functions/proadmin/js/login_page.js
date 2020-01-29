891e4a9d0603e3c8bdf05e05aec350adca20728e
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-01-28 21:19:21 -0600

    design login_page && create stylesheet for login_page() format

--- functions/proadmin/js/login_page.js
+++ functions/proadmin/js/login_page.js
@@ -1,3 +1,22 @@
+
+
 function login_page(){
-    glPageContent.innerHTML = '<h1>Login Page</h1>'
+    firebase.auth().onAuthStateChanged(user =>{
+        if(user && user.email==='prodadmin@test.com'){
+            window.location.href='/home'
+        }else {
+            glPageContent.innerHTML = `
+        <form class="form-signin">
+            <h3>Please Sign In </h3>
+            <input type="email" class="form-control" id="email" placeholder = "Email address">
+            <input type="password" class="form-control" id="password" placeholder = "Password">
+            <button type="submit" class="btn btn-primary">Submit</button>
+         </form>
+            
+            
+            `;
+        }
+    })
+
+
 }
