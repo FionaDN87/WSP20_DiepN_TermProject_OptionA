d23201e6f9854c99dcf088ea791e549f79a65e79
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-02-11 21:57:16 -0600

    Add card products in Show page

--- functions/proadmin/js/show_page.js
+++ functions/proadmin/js/show_page.js
@@ -1,3 +1,49 @@
 function show_page(){
-    glPageContent.innerHTML = '<h1>Show Page</h1>'
+    //glPageContent.innerHTML = '<h1>Show Page</h1>'
+    show_page_secured()
+   
+}
+//Global variable
+let products;   //list of all products read from db
+async function show_page_secured(){
+    glPageContent.innerHTML = '<h1>Show Products</h1>'
+
+    glPageContent.innerHTML+=`
+    <a href='/home' class="btn btn-outline-primary">Home</a>
+    <a href='/add' class="btn btn-outline-primary">Add a Products</a>
+    <br/>
+    `;
+    try{
+        products = []   //array of products
+        const snapshot = await firebase.firestore().collection(COLLECTION).get()
+        snapshot.forEach(doc =>{
+            const{name,summary,price,image,image_url} = doc.data()
+            const p = {docId: doc.id,name,summary,price, image,image_url}
+            products.push(p)
+        })
+    }catch (e){
+        glPageContent.innerHTML='Forestore access error. Try again!!!!' + e
+        return
+    }
+    //console.log(products)
+
+    if (products.length === 0){
+        glPageContent += '<h1>No products in the database</h1>'
+        return
+    }
+
+    for (let index = 0; index < products.length; index++){
+        const p = products[index]
+        glPageContent.innerHTML +=`
+        <div id="${p.docId}" class="card" style="width: 18rem; display: inline-block">
+            <img src="${p.image_url}" class="card-img-top" >
+            <div class="card-body">
+            <h5 class="card-title">${p.name}</h5>
+            <p class="card-text">${p.price}<br/>${p.summary}</p>
+          
+            </div>
+        </div>
+        
+        `;
+    }
 }
