10148559cf6ecbb9272b75fe0ea7ae02deecb1b5
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-03-11 14:58:10 -0500

    Added key in Function folder to user admin API

--- functions/adminUtil.js
+++ functions/adminUtil.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./diepn-wsp20-firebase-adminsdk-39m6c-8b83a9963b.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
